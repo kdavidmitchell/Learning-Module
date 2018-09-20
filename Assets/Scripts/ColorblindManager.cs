6b1f2fb37b1b2657072f27b00cee4ef032413231
kdavidmitchell <kdavidmitchell@gmail.com>
2018-09-20 11:47:25 -0400

    starting to apply colorblind mode changes

--- Assets/Scripts/ColorblindManager.cs
+++ Assets/Scripts/ColorblindManager.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ColorblindManager : MonoBehaviour 
 {
 
 	private static ColorblindManager instance = null;
+
+	public Color darkBG;
+	public Color lightBG;
+	public Color darkBorder;
+	public Color lightBorder;
+	public Color highlight;
+	public Material skyboxCB;
 
 	// Use this for initialization
 	void Awake() 
@@ -22,7 +30,34 @@ public class ColorblindManager : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
+	}
+
+	private void OnLevelWasLoaded()
+	{
+		if (GameInformation.ColorblindMode)
+		{
+			GameObject[] lightBorderObj = GameObject.FindGameObjectsWithTag("LightBorder");
+			for (int i = 0; i < lightBorderObj.Length; i++) 
+			{
+				lightBorderObj[i].GetComponent<Image>().color = lightBorder;
+			}
+
+			GameObject[] lightBGObj = GameObject.FindGameObjectsWithTag("LightBG");
+			for (int i = 0; i < lightBGObj.Length; i++) 
+			{
+				if (lightBGObj[i].GetComponent<Button>() != null)
+				{
+					ColorBlock buttonColors = lightBGObj[i].GetComponent<Button>().colors;
+					buttonColors.normalColor = lightBG;
+					lightBGObj[i].GetComponent<Button>().colors = buttonColors;
+				} else if (lightBGObj[i].GetComponent<Image>() != null)
+				{
+					lightBGObj[i].GetComponent<Image>().color = lightBG;
+				}
+			}
+		}
 	}
 }
