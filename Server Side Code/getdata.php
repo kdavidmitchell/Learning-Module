8a0fa4f14d9d3f3e0a04b16ef1ce8155ca8883cf
kdavidmitchell <kdavidmitchell@gmail.com>
2019-03-25 15:31:28 -0400

    template server and client code

---
+++ Server Side Code/getdata.php
@@ -0,0 +1,26 @@
+<?php
+ header("Access-Control-Allow-Credentials: true");
+ header('Access-Control-Allow-Origin: *');
+ header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+ header('Access-Control-Allow-Headers: Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time');
+ $servername = "sqldatabaseurl"; 
+ $server_username = "sqllogin"; 
+ $server_password = "sqlpassword"; 
+ $dbName = "DatabaseName";
+  
+  //make connection
+  $conn = new mysqli($servername, $server_username, $server_password, $dbName);
+  if(!conn){
+    die("Connection failed: " . mysqli_connect_error());
+  }
+ 
+    $query = "SELECT * FROM `tablename` ORDER by `score` DESC LIMIT 10";
+    $result = mysqli_query($conn, $query);
+  
+  if(mysqli_num_rows($result) > 0){
+    //show data for each row
+    while($row = mysqli_fetch_assoc($result)){
+      echo " Name: ".$row['name']. " - Score: ".$row['score']."\n";
+    }
+  }
+?>
---
+++ Server Side Code/postdata.php
@@ -0,0 +1,34 @@
+<?php 
+  header("Access-Control-Allow-Credentials: true");
+  header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+  header('Access-Control-Allow-Headers: Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time');
+  $servername = "sqldatabaseurl";
+  $server_username =  "sqllogin";
+  $server_password = "sqlpassword";
+  $dbName = "DatabaseName";
+  
+    // Strings must be escaped to prevent SQL injection attack. 
+    $name = $_POST["namePost"];
+    $score = $_POST["scorePost"]; 
+    $hash = $_POST["hashPost"];
+  //make connection
+  $conn = new mysqli($servername, $server_username, $server_password, $dbName);
+  if(!conn){
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  
+  $secretKey="insertsecretcodehere";
+  $real_hash = md5($name . $score . $secretKey);
+  
+  if($real_hash == $hash){
+    $sql = "INSERT INTO SuperBao_SP_Easy (name, score)
+      VALUES ('".$name."','".$score."')";
+      $result = mysqli_query($conn ,$sql);
+  }
+  
+  //Check Connection
+  if(!$conn){
+    die("Connection Failed. ". mysqli_connect_error());
+  }
+?>
